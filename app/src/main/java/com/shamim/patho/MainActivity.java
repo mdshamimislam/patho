e59f7ae4a33d97543d6d5af8438551d0c4b9002f
md_sh <mdshamimislam487@gmail.com>
2020-07-05 11:09:50 +0600

    Initial commit

---
+++ app/src/main/java/com/shamim/patho/MainActivity.java
@@ -0,0 +1,57 @@
+package com.shamim.patho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView name, password;
+    ProgressDialog progressDialog;
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        name = findViewById(R.id.name);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Name = name.getText().toString();
+                String Password = password.getText().toString();
+
+                if (Name.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please Enter Your User Name", Toast.LENGTH_SHORT).show();
+
+                } else if (Password.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please Enter Password", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    if (Name.equals("bangladesh") && Password.equals("123")) {
+
+
+
+                        Intent intent = new Intent(MainActivity.this, Homepage.class);
+                        startActivity(intent);
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "UserName = bangladesh and password= 123", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+
+    }
+}
